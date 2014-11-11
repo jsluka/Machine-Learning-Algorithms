7de901645ff4f0c1be5eb20ae0250021d0a3e94a
Justin Sluka <Sprtn137fl@gmail.com>
2014-11-10 22:03:15 -0500

    Linear Regression
    Begun the linear regression implementation, but the current version
    diverges to infinity.
    
    Dunno why.

---
+++ Linear Regression/linearRegression.py
@@ -0,0 +1,81 @@
+import numpy as np
+import sys,csv,math
+
+class linearRegression:
+    def __init__(self,trainingDataFilename,testDataFilename,numIters,learnRate):
+        self.trainFile = trainingDataFilename
+        self.testFile = testDataFilename
+        self.Iters = numIters
+        self.n = learnRate
+        self.w = []   # list of weights
+        self.b = 0    # bias
+        self.x = []   # data table
+        self.tX = []  # test data
+        self.y = []   # outputs
+        self.tY = []  # test outputs
+        np.seterr(divide="ignore",invalid="ignore")
+        self.operations()
+
+    # Logic
+    def operations(self):
+        self.readData()    # read in the data
+        self.learnModel()  # learn the model
+        self.squaredLoss() # test the model
+
+    # Learn the Regression Model
+    def learnModel(self):
+        yHat, e = 0, 0
+        for it in range(0,self.Iters):
+            for i in range(0,len(self.x)):
+                diff = self.y[i] - np.dot(self.w,self.x[i])
+                for j in range(0,len(self.w)):
+                    self.w[j] = self.w[j] + self.n*diff*self.x[i][j]
+                # endfor
+            # endfor
+        # endfor
+
+                
+    # Test the Regression Model
+    def squaredLoss(self):
+        yHat = 0
+        squaredSum = 0
+        for i in range(0,len(self.tX)):
+            squaredSum += np.dot(self.w,self.tX[i])**2
+        # endfor
+        ratio = float(squaredSum) / float(len(self.tX))
+        print("SQUARED LOSS = %f"%(ratio))
+        print("Learning is fun!")
+
+    # Read the .CSV files
+    def readData(self):
+        f = open(self.trainFile,'rt')
+        reader = csv.reader(f)
+        unparsedTrimmed = []
+        for row in reader: # Read in contents
+            unparsedTrimmed.append(row)
+        # endfor
+        f.close()
+        for row in range(0,len(unparsedTrimmed)):
+            self.x.append(unparsedTrimmed[row][:-1])
+            self.x[row] = list(map(float,self.x[row]))
+            self.y.append(unparsedTrimmed[row][-1])
+        # endfor
+        self.y = list(map(float,self.y))
+        
+        for x in range(0,len(self.x[0])):
+            self.w.append(0)
+        # endfor
+
+        f = open(self.testFile,'rt')
+        reader = csv.reader(f)
+        unparsedTrimmed = []
+        for row in reader: # Read in contents
+            unparsedTrimmed.append(row)
+        # endfor
+        f.close()
+        for row in range(0,len(unparsedTrimmed)):
+            self.tX.append(unparsedTrimmed[row][:-1])
+            self.tX[row] = list(map(float,self.tX[row]))
+            self.tY.append(unparsedTrimmed[row][-1])
+        # endfor
+        self.tY = list(map(float,self.tY))
